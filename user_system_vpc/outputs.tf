302d9823efc5cd63fc1171d102c800e3a6abd104
Augustine-kyei <68449181+Augustine-kyei@users.noreply.github.com>
2021-04-16 20:45:22 +0100

    Delete outputs.tf

--- user_system_vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_Security_Group" {
-value = aws_cloudhsm_v2_cluster.cloudhsm_v2_cluster.security_group_id
-
-}
-
-output "hsm_cluster_id" {
-  value = aws_cloudhsm_v2_cluster.cloudhsm_v2_cluster.cluster_id
-}
-
